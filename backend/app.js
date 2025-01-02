7a90aa60313e1249ec425313d0abcf2f0c6372f9
axelle <kadance9720@gmail.com>
2025-01-03 00:15:33 +0100

    Add mongo and mongoose for database

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.use((req, res) => {
-  res.json({ message: "Requête reçue !" });
-});
+require("dotenv").config();
+
+const mongoUri = `mongodb+srv://${process.env.Mongo_ID}:${process.env.Mongo_MP}@cluster0.ruqtd.mongodb.net/${process.env.Mongo_BDName}?retryWrites=true&w=majority&appName=Cluster0`;
+console.log("MongoDB URI:", mongoUri);
+mongoose
+  .connect(mongoUri)
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch((error) =>
+    console.error("Connexion à MongoDB échouée :", error.message)
+  );
 
 module.exports = app;
