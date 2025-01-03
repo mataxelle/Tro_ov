3424f9f0d2283caca81de112631f11b304d4c0c3
axelle <kadance9720@gmail.com>
2025-01-03 14:21:46 +0100

    Created object model

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,10 @@ const app = express();
 
 require("dotenv").config();
 
+app.get("/", (req, res) => {
+  res.send("Hello from Node API Server Updated");
+});
+
 const mongoUri = `mongodb+srv://${process.env.Mongo_ID}:${process.env.Mongo_MP}@cluster0.ruqtd.mongodb.net/${process.env.Mongo_BDName}?retryWrites=true&w=majority&appName=Cluster0`;
 console.log("MongoDB URI:", mongoUri);
 mongoose
