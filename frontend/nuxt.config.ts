4e59b3fbc3ca8877a3d4a225be5c0cbb89b21ce6
axelle <kadance9720@gmail.com>
2025-02-04 16:04:16 +0100

    Start use env variables

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,7 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-
+  runtimeConfig: {
+    public: {
+      apiUrl: process.env.NUXT_PUBLIC_API_BASE,
+    },
+  },
   build: {
     transpile: ['@popperjs/core', 'bootstrap']
   },
--- frontend/stores/auth.ts
+++ frontend/stores/auth.ts
@@ -1,94 +1,102 @@
-import { defineStore } from 'pinia';
+import { defineStore } from "pinia";
 
 interface User {
-    id: string;
-    email: string;
+  id: string;
+  email: string;
 }
 
 export interface AuthState {
-    isAuthenticated: boolean; // Indicates whether the user is logged in
-    user: User | null;
-    authToken: string | null;
+  isAuthenticated: boolean; // Indicates whether the user is logged in
+  user: User | null;
+  authToken: string | null;
+  apiBaseUrl: string;
 }
 
-export const useAuthStore = defineStore('auth', {
-    state: (): AuthState => ({
-        isAuthenticated: false,
-        user: null,
-        authToken: null,
-    }),
+export const useAuthStore = defineStore("auth", {
+  state: (): AuthState => {
+    const config = useRuntimeConfig();
+    return {
+      isAuthenticated: false,
+      user: null,
+      authToken: null,
+      apiBaseUrl: config.public.apiUrl as string, // Initialization as soon as the store is created
+    };
+  },
 
-    actions: {
-        async register(name: string, email: string, password: string) {
-            try {
-                const response = await $fetch<{
-                    user: User,
-                    token: string
-                }>("http://localhost:5000/api/auth/signUp", {
-                    method: "POST",
-                    body: { name, email, password },
-                });
+  actions: {
+    async register(name: string, email: string, password: string) {
+      try {
+        const response = await $fetch<{
+          user: User;
+          token: string;
+        }>(`${this.apiBaseUrl}/auth/signUp`, {
+          method: "POST",
+          body: { name, email, password },
+        });
 
-                this.user = response.user;
-            } catch (error) {
-                console.error('Registration error : ', error);
-                throw error;
-            }
-        },
-
-        async login(email: string, password: string) {
-            try {
-                const response = await $fetch<{
-                    user: User,
-                    token: string
-                }>("http://localhost:5000/api/auth/signIn", {
-                    method: "POST",
-                    body: { email, password },
-                    credentials: "include",
-                });
-
-                this.isAuthenticated = true;
-                this.user = response.user;
-                this.authToken = response.token;
-            } catch (error) {
-                console.error('Login error : ', error);
-                throw error;
-            }
-        },
-
-        async checkAuth() {
-            try {
-                const response = await $fetch<{
-                    isAuthenticated: boolean,
-                    user: User
-                }>("http://localhost:5000/api/auth/authCheck", {
-                    method: "GET",
-                    credentials: "include",
-                });
-
-                this.isAuthenticated = response.isAuthenticated;
-                this.user = response.user;
-            } catch (error) {
-                console.error('Authentification error : ', error);
-                this.logout();
-            }
-        },
-
-        async logout() {
-            try {
-                await $fetch("http://localhost:5000/api/auth/signOut", {
-                    method: "POST",
-                    credentials: "include",
-                });
-            } catch (error) {
-                console.error('Logout error : ', error);
-            } finally {
-                this.isAuthenticated = false;
-                this.user = null;
-                this.authToken = null;
-            }
-        },
+        this.user = response.user;
+      } catch (error) {
+        console.error("Registration error : ", error);
+        throw error;
+      }
     },
 
-    persist: true,
+    async login(email: string, password: string) {
+      try {
+        const response = await $fetch<{
+          user: User;
+          token: string;
+        }>(`${this.apiBaseUrl}/auth/signIn`, {
+          method: "POST",
+          body: { email, password },
+          credentials: "include",
+        });
+
+        this.isAuthenticated = true;
+        this.user = response.user;
+        this.authToken = response.token;
+      } catch (error) {
+        console.error("Login error : ", error);
+        throw error;
+      }
+    },
+
+    async checkAuth() {
+      try {
+        const response = await $fetch<{
+          isAuthenticated: boolean;
+          user: User;
+        }>(`${this.apiBaseUrl}/auth/authCheck`, {
+          method: "GET",
+          credentials: "include",
+        });
+
+        this.isAuthenticated = response.isAuthenticated;
+        this.user = response.user;
+      } catch (error) {
+        console.error("Authentification error : ", error);
+        this.logout();
+      }
+    },
+
+    async logout() {
+      try {
+        await $fetch(`${this.apiBaseUrl}/auth/signOut`, {
+          method: "POST",
+          credentials: "include",
+        });
+      } catch (error) {
+        console.error("Logout error : ", error);
+      } finally {
+        this.isAuthenticated = false;
+        this.user = null;
+        this.authToken = null;
+
+        // Reassigning `apiBaseUrl` to prevent it from becoming empty
+        this.$reset();
+      }
+    },
+  },
+
+  persist: true,
 });
